62a7fc27f9b0f31beacc5b50d51599372e8d6ecb
Alan P Santos <alan.ps_tec@outlook.com>
2023-05-12 09:28:39 -0300

    Atualizando

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu'); 
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
+        document.querySelector('.icon').scr = "assets/img/Carol.jpeg";
     } else {
         menuMobile.classList.sdd('open');
     }
